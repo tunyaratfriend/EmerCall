2faa9ce152ab186b2aef0dab37621a9df74ce744
Friend <tunyaratfriend10@gmail.com>
2018-02-19 14:36:35 +0700

    Initial commit

---
+++ app/src/main/java/friend/snru/ac/th/emercall/MainActivity.java
@@ -0,0 +1,13 @@
+package friend.snru.ac.th.emercall;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
