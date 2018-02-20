dabc7e214a9a7a71d250cebf13930598c5f8e65e
Friend <tunyaratfriend10@gmail.com>
2018-02-20 12:01:06 +0700

    Add Fragment Success

--- app/src/main/java/friend/snru/ac/th/emercall/MainActivity.java
+++ app/src/main/java/friend/snru/ac/th/emercall/MainActivity.java
@@ -3,11 +3,27 @@ package friend.snru.ac.th.emercall;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import friend.snru.ac.th.emercall.fragrament.MainFragment;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+        // Add Fragment
+        if (savedInstanceState == null) {
+
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.contentMainFragment, new MainFragment()).commit();
+
+
+        }
+
+
+
+
+    }   //Main Method
+
+}   //Main Class
