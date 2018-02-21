f178c22360537c6117df89fc42515744f7692017
Friend <tunyaratfriend10@gmail.com>
2018-02-21 11:25:48 +0700

    Drawer Menu Success

--- app/src/main/java/friend/snru/ac/th/emercall/MainActivity.java
+++ app/src/main/java/friend/snru/ac/th/emercall/MainActivity.java
@@ -1,23 +1,47 @@
 package friend.snru.ac.th.emercall;
 
+import android.content.res.Configuration;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 import friend.snru.ac.th.emercall.fragrament.MainFragment;
 
 public class MainActivity extends AppCompatActivity {
+
+//    Explicit
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle actionBarDrawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+//        Create Toolbar
+        Toolbar toolbar = findViewById(R.id.toolbarMain);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        drawerLayout = findViewById(R.id.drawerLayoutMain);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(
+               MainActivity.this,
+                drawerLayout,
+                R.string.open,
+                R.string.close
+        );
+        drawerLayout.setDrawerListener(actionBarDrawerToggle);
+
 //      Add Fragment
         if (savedInstanceState == null) {
 
             getSupportFragmentManager().beginTransaction()
                     .add(R.id.contentMainFragment, new MainFragment()).commit();
-
 
         }
 
@@ -26,4 +50,32 @@ public class MainActivity extends AppCompatActivity {
 
     }   //Main Method
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
+        return true;
+
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+
+        actionBarDrawerToggle.onConfigurationChanged(newConfig);
+
+    }
+
+    @Override
+    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+
+        actionBarDrawerToggle.syncState();
+
+
+
+    }
 }   //Main Class
